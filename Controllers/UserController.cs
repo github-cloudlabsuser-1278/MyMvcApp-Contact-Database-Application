fd0485da0c3ec96b75e4a5e12d779615fdf759c7
github-cloudlabsuser-1278 <168015974+github-cloudlabsuser-1278@users.noreply.github.com>
2024-12-18 09:13:37 +0000

    Enhance UserController with CRUD functionality and validation checks

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -12,17 +12,25 @@ public class UserController : Controller
         public ActionResult Index()
         {
             // Implement the Index method here
+        return View(userlist);
         }
 
         // GET: User/Details/5
         public ActionResult Details(int id)
         {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
             // Implement the details method here
         }
 
         // GET: User/Create
         public ActionResult Create()
         {
+            return View();
             //Implement the Create method here
         }
 
@@ -30,12 +38,24 @@ public class UserController : Controller
         [HttpPost]
         public ActionResult Create(User user)
         {
+            if (ModelState.IsValid)
+            {
+                userlist.Add(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
             // Implement the Create method (POST) here
         }
 
         // GET: User/Edit/5
         public ActionResult Edit(int id)
         {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
             // This method is responsible for displaying the view to edit an existing user with the specified ID.
             // It retrieves the user from the userlist based on the provided ID and passes it to the Edit view.
         }
@@ -44,16 +64,33 @@ public class UserController : Controller
         [HttpPost]
         public ActionResult Edit(int id, User user)
         {
-            // This method is responsible for handling the HTTP POST request to update an existing user with the specified ID.
-            // It receives user input from the form submission and updates the corresponding user's information in the userlist.
-            // If successful, it redirects to the Index action to display the updated list of users.
-            // If no user is found with the provided ID, it returns a HttpNotFoundResult.
-            // If an error occurs during the process, it returns the Edit view to display any validation errors.
+            var existingUser = userlist.FirstOrDefault(u => u.Id == id);
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                existingUser.Name = user.Name;
+                existingUser.Email = user.Email;
+                // Update other properties as needed
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(user);
         }
 
         // GET: User/Delete/5
         public ActionResult Delete(int id)
         {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
             // Implement the Delete method here
         }
 
@@ -61,6 +98,14 @@ public class UserController : Controller
         [HttpPost]
         public ActionResult Delete(int id, IFormCollection collection)
         {
+            var user = userlist.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            userlist.Remove(user);
+            return RedirectToAction(nameof(Index));
             // Implement the Delete method (POST) here
         }
 }
